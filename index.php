14b3074139ec233c6af8274b3a539977e718a851
kalauer <kalauer@riseup.net>
2017-01-12 22:25:02 +0100

    ajax

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<link rel="stylesheet" href="css/style.css" type="text/css" />
-	<script src="js/jquery.js" type="text/javascript"></script>
+	<script src="js/logic.js" type="text/javascript"></script>
 </head>
 <body>
 	<?php
@@ -12,23 +12,7 @@
 	<main>
 		<article>
 			<?php
-				//Die richtige Seite öffnen
-				if (!empty($_GET['p'])) {
-					$p = $_GET['p'];
-				} else {
-					$p = "allgemeines";
-				}
-				if ($p == "allgemeines" ||
-				$p == "programm" ||
-				$p == "haus" ||
-				$p == "kosten" ||
-				$p == "anfahrt" ||
-				$p == "anmeldung" ||
-				$p == "kontakt")	{
-					include("content/".$p.".inc");
-				} else {
-					include("content/404.inc");
-				}
+				include("content/allgemeines.inc");
 			?>
 		</article>
 	</main>
