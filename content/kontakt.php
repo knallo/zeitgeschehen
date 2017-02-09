7d7c5fda20a8f460eed9b8a947d0e200774a329d
Hansi <yoghurt@riseup.net>
2017-02-09 23:32:43 +0100

    Feinschliff

--- content/kontakt.php
+++ content/kontakt.php
@@ -2,10 +2,14 @@
 	Kontakt
 </h1>
 <p>
+	Die Veranstaltung wird organisiert vom <a href="http://arbeitskreisaufloesen.blogsport.eu" target="blank" title="Zur Website des Arbeitskreis Auflösen">Arbeitskreis Auflösen</a>.
+	<br />
 	Wenn es noch Fragen gibt, die nicht auf dieser Website beantwortet werden, sind wir kontaktierbar unter <a href="mailto:zeitgeschehen@riseup.net">zeitgeschehen@riseup.net</a>.
+	<br />
+	(Auch technische Probleme mit der Website gerne an die Mailadresse)
 </p>
 <p>
-	Außerdem könnt ihr euch in den <a href="https://lists.riseup.net/www/subscribe/zeitgeschehen" target="blank">Verteiler</a> eintragen.</p>
+	Außerdem könnt ihr euch in den <a href="https://lists.riseup.net/www/subscribe/zeitgeschehen" target="blank" title="Hier in den Verteiler eintragen">Verteiler</a> eintragen, über den alle Infos geschickt werden, die auch an die bei der Anmeldung angegebenen Mailadressen geschickt werden.</p>
 </p>
 <p>
 	Für rechtliche Fragen haben wir auch ein <a href="?p=impressum" title="Impressum">Impressum</a> eingerichtet.
--- content/workshop-auslaender.php
+++ content/workshop-auslaender.php
@@ -1,4 +1,4 @@
 <h1>
 	Ankündiger noch nicht fertig
 </h1>
-Der Ankündigungstext für diesen Workshop ist leider noch nicht fertig. Er wird in Kürze hier zu finden sein.
+<p>Der Ankündigungstext für diesen Workshop ist leider noch nicht fertig. Er wird in Kürze hier zu finden sein.</p>
