92c785d6c8d4352e60a735d911b90da8aae949c4
Hansi <yoghurt@riseup.net>
2017-02-10 11:39:23 +0100

    minor changes

--- content/kontakt.php
+++ content/kontakt.php
@@ -3,7 +3,8 @@
 </h1>
 <p>
 	Die Veranstaltung wird organisiert vom <a href="http://arbeitskreisaufloesen.blogsport.eu" target="blank" title="Zur Website des Arbeitskreis Auflösen">Arbeitskreis Auflösen</a>.
-	<br />
+</p>
+<p>
 	Wenn es noch Fragen gibt, die nicht auf dieser Website beantwortet werden, sind wir kontaktierbar unter <a href="mailto:zeitgeschehen@riseup.net">zeitgeschehen@riseup.net</a>.
 	<br />
 	(Auch technische Probleme mit der Website gerne an die Mailadresse)
