5b52893b58a5266d9f104196ef34c01e9a3da719
Hansi <yoghurt@riseup.net>
2017-02-15 12:04:07 +0100

    Neuer Ankündiger für AfD-Workshop

--- content/workshop-rechteKritisieren.php
+++ content/workshop-rechteKritisieren.php
@@ -1,10 +1,16 @@
 <h1>
 	Workshop: Wie man die Ausländerfeindlichkeit von AfD und Pegida kritisiert und wie nicht!
 </h1>
-<p>Dass die Flüchtlinge uns die Arbeitsplätze wegnehmen, das leuchtet auch so manchem von denen ein, die sich zu den Gegnern von AfD und Pegida rechnen und sich von Nationalismus und Rassismus distanzieren. Die Rechnung scheint ja auch sehr einfach zu sein: Je mehr Menschen auf dem Arbeitsmarkt um knappe Arbeitsplätze konkurrieren, desto schwerer wird es für jeden einzelnen Bewerber, sein Ziel zu erreichen. Aber geht es der ausländerfeindlichen Parole der AfD überhaupt darum, Leuten in ihren Konkurrenznöten zu helfen, wie immer die sie deuten? Die AfD will ja begründen, dass Ausländer, Flüchtlinge zumal, bei uns nichts verloren haben. Folglich wirft sie ganz andere Fragen auf:</p>
 <p>
+	Dass Flüchtlinge uns die Arbeitsplätze wegnehmen, leuchtet nicht nur bekennenden AfD-Anhängern ein. Die Rechnung scheint ja auch sehr einfach zu sein: Je mehr die Nachfrage nach Arbeitsplätzen das Angebot übersteigt, desto schwerer wird es für jeden einzelnen Bewerber, sein Ziel zu erreichen. Doch ist das eigentlich der Gehalt der Parole? Geht es den Ausländerfeinden von der AfD überhaupt darum, Leuten in ihren Konkurrenznöten zu helfen, wie immer die sie deuten?
+</p>
+<p>
+	Dass es sich um eine ungehörige, also rassistische und nationalistische Parole handelt, ist dagegen den meisten sofort klar. Sie wissen ja, wer sie in die Welt setzt! Worin aber der Nationalismus von AfD und Pegida besteht, wie er in der Parole vorkommt, wie man ihn kritisiert und was der mit Arbeitsplätzen zu tun hat, ist weniger klar. Einwände weisen in der Regel darauf hin, dass es doch auf dem deutschen Arbeitsmarkt viele offene Stellen gibt, Ausländer hier viel häufiger von Arbeitslosigkeit bedroht sind als Inländer und gelegentlich sogar Arbeitsplätze schaffen. Und das soll AfD-Anhänger überzeugen, die doch in jedem Flüchtling einen Anschlag auf Deutschland sehen?
+</p>
+<p>
+	Da ist einiges zu klären:
 	<ul>
-		<li>Was behauptet die AfD damit über die Konkurrenz?</li>
+		<li>Was ist in der Parole eigentlich über die Konkurrenz um Arbeitsplätze ausgesagt?</li>
 		<li>Welches Urteil fällt sie über Arbeitsplätze und den Arbeitsmarkt?</li>
 		<li>Worüber wird in der Konkurrenz denn tatsächlich entschieden?</li>
 		<li>Was behauptet das Urteil, dass Arbeitsplätze allein ein Besitzstand der Inländer sind?</li>
@@ -13,4 +19,3 @@
 		<li>Und was wird der Flüchtlingspolitik der Kanzlerin damit alles vorgeworfen?</li>
 	</ul>
 </p>
-<p>Diese und weitere Fragen zur Sache sollen auf dem Workshop in drei Sitzungen an einschlägigem Material erörtert werden.</p>
