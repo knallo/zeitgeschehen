1a8b16a4fd8129edc29408199afb3e838fae2a8b
Hansi <yoghurt@riseup.net>
2017-03-05 19:49:09 +0100

    Ankündiger für Inländer/Ausländer-Workshop hinzugefügt

--- content/workshop-auslaender.php
+++ content/workshop-auslaender.php
@@ -3,7 +3,18 @@ include("/home/webpages/lima-city/fruehlingsseminar/zeitgeschehen/includes/wsstr
 wsTitel("Inländer / Ausländer / Flüchtlinge", "Ein Werk der Gewalt");
 ?>
 
-<p><em>Der Ankündigungstext für diesen Workshop ist leider noch nicht fertig. Er wird in Kürze hier zu finden sein.</em></p>
+<p>In Deutschland sind die Meinungen darüber, wie man sich den Ausländern und ganz besonders den Flüchtlingen gegenüber benehmen sollte, geteilt. 
+	Gar nicht zur Sprache kommt dabei, dass da Leute als Inländer anderen als Ausländern gegenüberstehen – dieses Verhältnis wird einfach als Selbstverständlichkeit unterstellt. 
+	Ein Verhältnis, das sich keiner selbst aussuchen kann, auch die Deutschen nicht, die sich so engagiert in die Rolle 
+	des Inländers werfen und die Frage wälzen, was „gute“ oder „richtige“ Deutsche von Ausländern zu halten haben. 
+	Eine merkwürdige Selbstverständlichkeit ist das.</p>
+
+<p>Was dieses Verhältnis bedeutet, wollen wir auf unserem Workshop diskutieren: 
+	Was ist denn überhaupt die Scheidung von Inländern und Ausländern? 
+	Was ist ein Inländer und was steht mit diesem Status schon alles von vornherein über sein Leben fest? 
+	Was ist ein Ausländer? 
+	Wann, unter welchen Bedingungen und wie bekommen Inländer es überhaupt mit Ausländern zu tun? 
+	Und was für Ausländer sind „Merkels” Flüchtlinge eigentlich?</p>
 
 <?php
 wsZeiten(2);
