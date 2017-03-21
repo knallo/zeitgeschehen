504804f5fdbbf593ee4168d633f843352fac201e
Hansi <yoghurt@riseup.net>
2017-03-21 13:07:57 +0100

    added link to facebook

--- content/kontakt.php
+++ content/kontakt.php
@@ -5,9 +5,9 @@
 	Die Veranstaltung wird organisiert vom <a href="http://arbeitskreisaufloesen.blogsport.eu" target="blank" title="Zur Website des Arbeitskreis Auflösen">Arbeitskreis Auflösen</a>.
 </p>
 <p>
-	Wenn es noch Fragen gibt, die nicht auf dieser Website beantwortet werden, sind wir kontaktierbar unter <a href="mailto:zeitgeschehen@riseup.net">zeitgeschehen@riseup.net</a>.
-	<br />
-	(Auch technische Probleme mit der Website gerne an die Mailadresse)
+	Wenn es noch Fragen gibt, die nicht auf dieser Website beantwortet werden, sind wir kontaktierbar unter <a href="mailto:zeitgeschehen@riseup.net">zeitgeschehen@riseup.net</a>.<br />
+	(Auch technische Probleme mit der Website gerne an die Mailadresse.)<br />
+	Außerdem sind wir auch als <a href="https://www.facebook.com/akAufloesen/" target="blank">Arbeitskreis Auflösen auf Facebook</a> zu finden und erreichbar.
 </p>
 <p>
 	Für rechtliche Fragen haben wir auch ein <a href="?p=impressum" title="Impressum">Impressum</a> eingerichtet.
