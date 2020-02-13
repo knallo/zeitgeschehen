597c200fbfeba5a10ca1b45375223102e06ed457
Karl Auer <kalauer@riseup.net>
2020-02-13 18:58:05 +0100

    verbesserte ics datei

--- uploads/ics.php
+++ uploads/ics.php
@@ -16,15 +16,16 @@
     $workshop[] = $row["kuerzel"];
     if ($row["ist_erste_schiene"] == True) {
       $workshops_erste_schiene[] = $workshop;
-    } else {
+    } 
+    if ($row["ist_zweite_schiene"] == True) {
       $workshops_zweite_schiene[] = $workshop;
     }
   }
 
   $intro_string = "BEGIN:VCALENDAR\r\nVERSION:2.0\r\nX-WR-CALNAME:Argumente gegen das Zeitgeschehen %s\r\nCALSCALE:GREGORIAN\r\n";
   $timezone_string = "BEGIN:VTIMEZONE\r\nTZID:Europe/Berlin\r\nTZURL:http://tzurl.org/zoneinfo-outlook/Europe/Berlin\nX-LIC-LOCATION:Europe/Berlin\r\nBEGIN:DAYLIGHT\r\nTZOFFSETFROM:+0100\r\nTZOFFSETTO:+0200\r\nTZNAME:CEST\r\nDTSTART:19700329T020000\r\nRRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\r\nEND:DAYLIGHT\r\nBEGIN:STANDARD\r\nTZOFFSETFROM:+0200\r\nTZOFFSETTO:+0100\r\nTZNAME:CET\r\nDTSTART:19701025T030000\r\nRRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\r\nEND:STANDARD\r\nEND:VTIMEZONE\r\n";
-  $whole_event_string = "BEGIN:VEVENT\r\nDTSTAMP:%s\r\nDTSTART;VALUE=DATE:%s\r\nDTEND;VALUE=DATE:%s\r\nSUMMARY:Argumente gegen das Zeitgeschehen %s\r\nURL:https://www.zeitgeschehen.net\r\nDESCRIPTION:Workshopwochenende 'Argumente gegen das Zeitgeschehen %s'.\\n\\nAnreise ab %s gegen %s Uhr möglich.\\nFür Unterkunft und Verpflegung ist gesorgt\; Bettzeug muss mitgebracht werden.\\n\\n(Aktuelle) Infos:\\nwww.zeitgeschehen.net\\n\\nFür weitere Fragen:\\nzeitgeschehen@riseup.net\r\nLOCATION:Müllerweg 8 Barsinghausen OT Großgoltern 30890 Niedersachsen\r\nTRANSP:OPAQUE\r\nX-MICROSOFT-CDO-BUSYSTATUS:OOF\r\nEND:VEVENT\r\n";
-  $workshop_string = "BEGIN:VEVENT\r\nDTSTAMP:%s\r\nDTSTART;TZID='Europe/Berlin':%s\r\nDTEND;TZID='Europe/Berlin':%s\r\nSUMMARY:%s\r\nURL:%s\r\nDESCRIPTION:%s\r\nLOCATION:Müllerweg 8 Barsinghausen OT Großgoltern 30890 Niedersachsen\r\nTRANSP:OPAQUE\r\nX-MICROSOFT-CDO-BUSYSTATUS:OOF\r\nBEGIN:VALARM\r\nACTION:DISPLAY\r\nDESCRIPTION:%s\r\nTRIGGER:-PT30M\r\nEND:VALARM\r\nEND:VEVENT\r\n";
+  $whole_event_string = "BEGIN:VEVENT\r\nDTSTAMP:%s\r\nDTSTART;VALUE=DATE:%s\r\nDTEND;VALUE=DATE:%s\r\nSUMMARY:Argumente gegen das Zeitgeschehen %s\r\nURL:https://www.zeitgeschehen.net\r\nDESCRIPTION:Workshopwochenende 'Argumente gegen das Zeitgeschehen %s'.\\n\\nAnreise ab %s gegen %s Uhr möglich.\\nFür Unterkunft und Verpflegung ist gesorgt\; Bettzeug muss mitgebracht werden.\\n\\n(Aktuelle) Infos:\\nwww.zeitgeschehen.net\\n\\nFür weitere Fragen:\\nzeitgeschehen@riseup.net\r\nLOCATION:Brunsbergweg 6 Buchholz 21244 Hamburg\r\nTRANSP:OPAQUE\r\nX-MICROSOFT-CDO-BUSYSTATUS:OOF\r\nEND:VEVENT\r\n";
+  $workshop_string = "BEGIN:VEVENT\r\nDTSTAMP:%s\r\nDTSTART;TZID='Europe/Berlin':%s\r\nDTEND;TZID='Europe/Berlin':%s\r\nSUMMARY:%s\r\nURL:%s\r\nDESCRIPTION:%s\r\nLOCATION:Burnsbergweg 6 Buchholz 21244 Hamburg\r\nTRANSP:OPAQUE\r\nX-MICROSOFT-CDO-BUSYSTATUS:OOF\r\nBEGIN:VALARM\r\nACTION:DISPLAY\r\nDESCRIPTION:%s\r\nTRIGGER:-PT30M\r\nEND:VALARM\r\nEND:VEVENT\r\n";
 
   $now = time();
   $workshop_url = "http://localhost/zeitgeschehen/?p=%s";
@@ -116,7 +117,7 @@
   $ics = $ics . "END:VCALENDAR";
 
   header('Content-Disposition: attachment; filename="calendar.ics"');
-  header('Content-Type: text/plain');
+  header('Content-Type: text/calendar');
   header('Content-Length: ' . strlen($ics));
   header('Connection: close');
   echo $ics;
